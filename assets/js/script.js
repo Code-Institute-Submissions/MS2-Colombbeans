3bfcaa2e72e58bae2d8101f0f62b55a80072b572
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-01-07 18:51:28 +0000

    Add mobile nav menu and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //Function to animate the slideshow overlay from the home section
     $(".home-gallery > div:gt(0)").hide();
     setInterval(function () {
         $(".home-gallery > div:first")
@@ -8,4 +9,23 @@ $(document).ready(function () {
             .end(3000)
             .appendTo(".home-gallery");
     }, 5000);
+});
+
+//Function to make the hamburger button display the navigation menu
+const hamburger = document.querySelector(".hamburger");
+const mobileNavLinks = document.querySelector(".mobile-nav-links");
+const mobileLinks = document.querySelectorAll(".mobile-nav-links li");
+
+hamburger.addEventListener("click", () => {
+    mobileNavLinks.classList.toggle("open");
+    mobileLinks.forEach(link => {
+    link.classList.toggle("fade");
+    });
+});
+
+mobileNavLinks.addEventListener("click", () => {
+    mobileNavLinks.classList.toggle("open");
+    mobileLinks.forEach(link => {
+        link.classList.toggle("fade");
+    });
 });
