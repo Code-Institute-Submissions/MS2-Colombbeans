d8147aec773ae91fc43cce9139ad9aa510e81c7c
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-01-09 15:56:37 +0000

    Add code for JS transitions on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,33 @@ mobileNavLinks.addEventListener("click", () => {
         link.classList.toggle("fade");
     });
 });
+
+//Function for fade-in and slide-in animation effects
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px -250px 0px 0px"
+    };
+const appearOnScroll = new IntersectionObserver(function(
+    entries, 
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, 
+    appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
